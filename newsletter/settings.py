fa4f7b957696f6d8dffd1123a053befc859f83ba
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-28 18:04:36 +0530

    adding all the data

--- newsletter/settings.py
+++ newsletter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$633e^5*#-do54#vq@c1f4a9-jii=^p+s(se&gt3z1d#n3i)3l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
